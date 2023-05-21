e28374b6e6d567444d7bc51cf1725ba8745cbcfd
Ponchucha <ponchucha@yandex.ru>
2023-05-21 18:40:36 +0300

    edited saving orders according to srp

---
+++ spr2/Program.java
@@ -0,0 +1,14 @@
+
+
+public class Program {
+
+    //TODO: (Домашняя работа: 1) Сохранить заказ в XML документ
+    public static void main(String[] args) {
+        System.out.println("Введите заказ:");
+        Order order = new Order("", "", 0, 0);
+        order.inputFromConsole();
+        OrderSaver saver = new OrderSaver(order);
+        saver.SaveToXML();
+    }
+
+}
